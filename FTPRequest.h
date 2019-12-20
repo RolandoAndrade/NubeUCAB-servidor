b66eabfbc5617ce659e70a084c494a963d94df8e
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-12-20 18:38:53 -0400

    Respuesta FTP

--- FTPRequest.h
+++ FTPRequest.h
@@ -7,106 +7,106 @@ using namespace std;
 
 class FTPRequest 
 {
-private:
+	private:
 
-	string cmd;
-	string argv;
+		string cmd;
+		string argv;
 
-public:
+	public:
 
-	FTPRequest()
-	{
-
-	};
-	~FTPRequest()
-	{
-
-	}
-
-	FTPRequest(string c)
-	{
-		setRequest(c);
-	}
-
-	FTPRequest(string c,string args)
-	{
-		setRequest(c,args);
-	}
-
-	FTPRequest(string c, vector<string> flags)
-	{
-		setRequest(c,flags);
-	}
-
-	FTPRequest(string c, vector<string> flags, vector<string> args)
-	{
-		setRequest(c,flags,args);
-	}
-	
-	void setRequest(string c)
-	{
-		cmd = c;
-		argv = "";
-	}
-
-	void setRequest(string c,string args)
-	{
-		cmd = c;
-		argv = " "+args;
-	}
-
-	void setRequest(string c, vector<string> flags)
-	{
-		cmd = c;
-		argv = "";
-		for(string f: flags)
+		FTPRequest()
 		{
-			argv += " " + f;
+
+		};
+		~FTPRequest()
+		{
+
 		}
-	}
 
-	void setRequest(string c, vector<string> flags, vector<string> args)
-	{
-		setRequest(c,flags);
-		for(string f: args)
+		FTPRequest(string c)
 		{
-			argv += " " + f;
+			setRequest(c);
 		}
-	}
 
-	string getRequest()
-	{
-		if(argv == "")	
+		FTPRequest(string c,string args)
 		{
-			return cmd + "\r\n";
+			setRequest(c,args);
 		}
-		else if(cmd == "PORT")
+
+		FTPRequest(string c, vector<string> flags)
 		{
-			stringstream arg;
-			for(char c:argv)
+			setRequest(c,flags);
+		}
+
+		FTPRequest(string c, vector<string> flags, vector<string> args)
+		{
+			setRequest(c,flags,args);
+		}
+		
+		void setRequest(string c)
+		{
+			cmd = c;
+			argv = "";
+		}
+
+		void setRequest(string c,string args)
+		{
+			cmd = c;
+			argv = " "+args;
+		}
+
+		void setRequest(string c, vector<string> flags)
+		{
+			cmd = c;
+			argv = "";
+			for(string f: flags)
 			{
-				if(c=='.')
-				{
-					arg<<',';
-				}
-				if(c==':')
-				{
-					arg<<',';
-					int port = atoi(argv.substr(i+1).c_str());
-					arg<<port/256<<","<<port%256;
-					break;
-				}
-				else
-				{
-					//arg<<_cmd[i];
-					arg<<c;
-				}
+				argv += " " + f;
 			}
-			return cmd+" "+argv.str()+"\r\n";
 		}
-		else
+
+		void setRequest(string c, vector<string> flags, vector<string> args)
 		{
-			return cmd + argv +"\r\n";
+			setRequest(c,flags);
+			for(string f: args)
+			{
+				argv += " " + f;
+			}
 		}
-	}
+
+		string getRequest()
+		{
+			if(argv == "")	
+			{
+				return cmd + "\r\n";
+			}
+			else if(cmd == "PORT")
+			{
+				stringstream arg;
+				for(char c:argv)
+				{
+					if(c=='.')
+					{
+						arg<<',';
+					}
+					if(c==':')
+					{
+						arg<<',';
+						int port = atoi(argv.substr(i+1).c_str());
+						arg<<port/256<<","<<port%256;
+						break;
+					}
+					else
+					{
+						//arg<<_cmd[i];
+						arg<<c;
+					}
+				}
+				return cmd+" "+argv.str()+"\r\n";
+			}
+			else
+			{
+				return cmd + argv +"\r\n";
+			}
+		}
 };
