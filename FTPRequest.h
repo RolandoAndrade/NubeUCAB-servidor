e1ee8fac6d73bb0522fc9d600902f8c49cd731d3
rolandoandrade <rolandoandradefernandez@gmail.com>
2019-12-20 18:12:36 -0400

    Petición FTP sencilla

---
+++ FTPRequest.h
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+#include <sstream>
+#include <cstdlib>
+
+using namespace std;
+
+class FTPRequest 
+{
+private:
+
+	string cmd;
+	string argv;
+
+public:
+
+	FTPRequest()
+	{
+
+	};
+	~FTPRequest()
+	{
+
+	}
+
+	FTPRequest(string c)
+	{
+		cmd = c;
+		argv = "";
+	}
+	
+	FTPRequest(string,string);
+	FTPRequest(string, vector<string>);
+	FTPRequest(string, vector<string>, vector<string>);
+	
+	void setRequest(string);
+	void setRequest(string,string);
+	void setRequest(string, vector<string>);
+	void setRequest(string, vector<string>, vector<string>);
+	string getRequest();
+	string getRequest(string);
+
+};
